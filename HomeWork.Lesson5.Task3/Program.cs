ab9c971c0a27ab32cae1d5de92dd0e5290dfd40e
Александр Izumroot <izumroot@aistmail.com>
2021-02-26 14:19:27 +0600

    stack copy

---
+++ HomeWork.Lesson5.Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Author : Alexander Dolgosheev
+ * Github : https://github.com/dolgosheev
+ * Mailto : alexanderdolgosheev@gmail.com
+ * Task 5
+ */
+
+using System;
+using System.Collections;
+
+namespace HomeWork.Lesson5.Task3
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var stack = new Stack();
+         
+            stack.Push(0);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine($"Original Stack {string.Join(',', stack.ToArray())}");
+
+            var stackNCopy = (Stack) stack.Clone();
+
+            Console.WriteLine($"Stack Copy {string.Join(',', stackNCopy.ToArray())}");
+
+            Console.WriteLine("\nAdd 10 to stack and 30 to stackcopy\n");
+            stack.Pop();
+            stack.Push(10);
+            stackNCopy.Push(30);
+
+            Console.WriteLine($"Original Stack {string.Join(',', stack.ToArray())}");
+            Console.WriteLine($"Stack Copy {string.Join(',', stackNCopy.ToArray())}");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
