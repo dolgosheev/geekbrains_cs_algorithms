5df78db87ceb23992fdac4f495655e3a35bcc5e6
Александр Izumroot <izumroot@aistmail.com>
2021-02-26 13:23:22 +0600

    lesson 4

--- HomeWork.Lesson1.Task8/Program.cs
+++ HomeWork.Lesson1.Task8/Program.cs
@@ -42,9 +42,9 @@ namespace HomeWork.Lesson1.Task8
                 var difference = bI - aI;
                 Console.WriteLine($"a is {aI} | b is {bI} | interval is {++difference}");
 
-                for (var i = aI; i <= difference; i++)
+                for (var i = 0; i < difference; i++)
                 {
-                    Console.WriteLine($"Operation for digit {i} | ^2 = {Math.Pow(i,2)} | ^3 = {Math.Pow(i, 3)}");
+                    Console.WriteLine($"Operation for digit {aI + i} | ^2 = {Math.Pow((aI + i),2)} | ^3 = {Math.Pow((aI + i), 3)}");
                 }
             }
         }
